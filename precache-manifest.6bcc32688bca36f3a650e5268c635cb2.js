1deb1c1344618814ccb091b73b20030f042192a2
Denzel Hartshorn <denzelhartshorn@gmail.com>
2020-08-11 15:09:36 -0700

    Updates

---
+++ precache-manifest.6bcc32688bca36f3a650e5268c635cb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac389ca6db31eb2ac058",
+    "url": "/static/css/main.ac210ef9.chunk.css"
+  },
+  {
+    "revision": "ac389ca6db31eb2ac058",
+    "url": "/static/js/main.ac389ca6.chunk.js"
+  },
+  {
+    "revision": "6221052083d9d7c06dcf",
+    "url": "/static/js/1.62210520.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04b85977dc74a819f306d13241179157",
+    "url": "/index.html"
+  }
+];
